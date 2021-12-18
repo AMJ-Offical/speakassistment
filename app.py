630315157daa8562351327e524c81537917979a7
Phil7004 <69137110+Phil7004@users.noreply.github.com>
2021-12-18 18:41:26 +0100

    Add files via upload

---
+++ app.py
@@ -0,0 +1,39 @@
+# coding: UTF-8
+# moduls
+
+
+# code by Henrik und Philipp
+
+# Eingabe :
+
+
+
+# Ausgabe :
+
+
+
+# Verarbeitung des Textes
+
+
+#
+
+
+#
+
+
+#
+
+
+
+# Neue/n begriff erklären
+erklneuenbegriff = "Ich erklär dir einen nuen begriff"
+
+while True:
+    input1 = input(" Eingabe")
+    input1 = input1.lower()
+    listinput1 = input1.split()
+    leninput1 = len(input1)
+    lenlistinput1 = len(listinput1)
+    if "Ich erklär dir ein neues wort" or "Ich erklär dir einen nueen begriff" == "x":
+        print("b")
+
